2ae7830c56ec6652bc89f69c68581ba73ffca2c2
saikiranreddyappidi <saikiranreddyvfstr@gmail.com>
2023-08-31 13:34:54 +0530

    updated g(n)

--- AI/A-star.py
+++ AI/A-star.py
@@ -44,21 +44,25 @@ class Utility:
         for i in temp:
             if i != self.grid_obj.prev and self.grid_obj.info(i):
                 self.arr.append(i)
-                costs.append(self.h_n(i) + self.h_n(i))
+                costs.append(self.g_n(i) + self.h_n(i))
             if i == self.grid_obj.goal:
                 self.arr.append(i)
-                costs.append(self.h_n(i) + self.h_n(i))
+                costs.append(self.g_n(i) + self.h_n(i))
                 print("Goal Reached")
                 return i
-        next_step = self.arr[costs.index(min(costs))]
-        print("prev", self.grid_obj.prev, "current", self.current, "Next step: ", next_step)
+        try:
+            next_step = self.arr[costs.index(min(costs))]
+            print("Prev: ", self.grid_obj.prev, "Current: ", self.current, "Next Step: ", next_step)
+        except ValueError:
+            print("No valid moves left.")
+            exit(2)
         self.grid_obj.prev = self.current
         self.arr.clear()
         return next_step
 
     def g_n(self, to: tuple) -> int:
         var = (self.current, to)
-        cost = self.pathTracker[var] = randint(0, 25)
+        cost = self.pathTracker[var] = 1# randint(0, 25)
         return cost
 
     def h_n(self, to: tuple) -> float:
