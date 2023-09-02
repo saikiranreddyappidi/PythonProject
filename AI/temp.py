bc15bbeb9443728e8e47c84b77917ad7f9fa81c5
saikiranreddyappidi <saikiranreddyvfstr@gmail.com>
2023-09-02 12:56:35 +0530

    AO CD

--- AI/temp.py
+++ AI/temp.py
@@ -1,112 +1,83 @@
-from random import randint
-from math import dist
+class PredictiveParsingTable:
+    def __init__(self, grammar):
+        self.grammar = grammar
+        self.non_terminals = grammar.keys()
+        self.terminals = set()
+        self.parsing_table = {}
+
+        for productions in grammar.values():
+            for production in productions:
+                for symbol in production:
+                    if symbol not in self.non_terminals:
+                        self.terminals.add(symbol)
+
+        for non_terminal in self.non_terminals:
+            self.parsing_table[non_terminal] = {}
+
+        for non_terminal, productions in grammar.items():
+            for production in productions:
+                first_set = self.compute_first_set(production)
+                for terminal in first_set:
+                    self.parsing_table[non_terminal][terminal] = production
+
+                if '' in first_set:
+                    follow_set = self.compute_follow_set(non_terminal)
+                    for terminal in follow_set:
+                        self.parsing_table[non_terminal][terminal] = production
+
+    def compute_first_set(self, production):
+        first_set = set()
+        symbol = production[0]
+        if symbol in self.terminals:
+            first_set.add(symbol)
+        else:
+            first_set.update(self.compute_first_set(self.grammar[symbol][0]))
+
+        return first_set
+
+    def compute_follow_set(self, non_terminal):
+        follow_set = set()
+        if non_terminal == 'S':
+            follow_set.add('$')
+
+        for nt, productions in self.grammar.items():
+            for production in productions:
+                if non_terminal in production:
+                    idx = production.index(non_terminal)
+                    if idx < len(production) - 1:
+                        follow_set.update(self.compute_first_set(production[idx + 1:]))
+                    elif idx == len(production) - 1:
+                        if nt != non_terminal:
+                            follow_set.update(self.compute_follow_set(nt))
+
+        return follow_set
+
+    def print_parsing_table(self):
+        for non_terminal, terminals in self.parsing_table.items():
+            print(f'Non-terminal {non_terminal}:')
+            for terminal, production in terminals.items():
+                print(f'  Terminal {terminal}: {production}')
 
 
-class grid:
-    def __init__(self):
-        print("Grid environment construction initialized. Only binary values allowed, 0 represents blocked and 1 "
-              "represents free grid.")
-        row, col = map(int, input("Enter no.of rows and columns: ").split())
-        self.graph = []
-        for i in range(row):
-            self.graph.append(list(map(int, input("Enter " + str(i) + " row: ").split())))
-        print(self.graph)
-        print("Input the coordinates of starting grid and goal grid")
-        self.start = tuple(map(int, input("Enter start grid coordinates: ").split()))
-        self.goal = tuple(map(int, input("Enter goal grid coordinates: ").split()))
-        self.prev = None
-        self.path_cost = {}
-        self.cost()
+# Grammar G1: S->AaDd|DdDa
+grammar_g1 = {
+    'S': ['AaDd', 'DdDa'],
+    'A': ['a'],
+    'D': ['d']
+}
 
-    def info(self, coordinates: tuple) -> bool:
-        try:
-            if self.graph[coordinates[0]][coordinates[1]] == 1 and coordinates[0] > -1 and coordinates[1] > -1:
-                return True
-            else:
-                return False
-        except IndexError:
-            print("Invalid Coordinates", coordinates)
-            return False
+# Grammar G2: AaBaAbBb|BbAaBaAb
+grammar_g2 = {
+    'S': ['AaBaAbBb', 'BbAaBaAb'],
+    'A': ['a'],
+    'B': ['b']
+}
 
-    def cost(self):
-        rows = len(self.graph)
-        col = len(self.graph[0])
-        linear = []
-        for i in range(rows):
-            for j in range(col):
-                linear.append((i, j))
-        k = 0
-        for i in linear:
-            self.path_cost[i] = randint(5, 55 - k) if i != self.goal else 0
-            k += 1
+pp_table_g1 = PredictiveParsingTable(grammar_g1)
+pp_table_g2 = PredictiveParsingTable(grammar_g2)
 
-    def get_cost(self, frm: tuple) -> int:
-        return self.path_cost[frm]
+print("Predictive Parsing Table for Grammar G1:")
+pp_table_g1.print_parsing_table()
 
-
-class Utility:
-    def __init__(self, grid_obj: grid):
-        self.current = ()
-        self.cost = 0
-        self.pathTracker = {}
-        self.grid_obj = grid_obj
-        self.closed = []
-        self.open = [self.grid_obj.start]
-        # self.prev = self.current
-
-    def next_move(self, current: tuple) -> tuple:
-        x, y = self.current = current
-        temp = [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1), (x, y - 1), (x, y + 1), (x + 1, y - 1), (x + 1, y),
-                (x + 1, y + 1)]
-        for i in temp:
-            if i != self.grid_obj.prev and self.grid_obj.info(i) and i not in self.closed:
-                self.open.append(i)
-            if i == self.grid_obj.goal:
-                self.open.append(i)
-                print("Goal Reached")
-                return i
-        next_step = ()
-        min_cost = 100
-        for i in self.open:
-            if self.grid_obj.get_cost(i) < min_cost:
-                next_step = i
-                min_cost = self.grid_obj.get_cost(i)
-        self.closed.append(current)
-        print("prev", self.grid_obj.prev, "current", self.current, "Next step: ", next_step)
-        self.open.clear()
-        self.grid_obj.prev = self.current
-        return next_step
-
-    def g_n(self, to: tuple) -> int:
-        var = (self.current, to)
-        cost = self.pathTracker[var] = randint(0, 25)
-        return cost
-
-    def h_n(self, to: tuple) -> float:
-        if to == self.grid_obj.goal:
-            return 0
-        return dist(to, self.grid_obj.goal)
-
-
-def main():
-    grid_obj = grid()
-    util_obj = Utility(grid_obj)
-    util_obj.current = grid_obj.start
-    final_path = [grid_obj.start]
-    while True:
-        if util_obj.current == grid_obj.goal:
-            break
-        util_obj.current = util_obj.next_move(util_obj.current)
-        final_path.append(util_obj.current)
-        print("Current: ", util_obj.current)
-    print("Path: ", final_path)
-    print("Closed: ", util_obj.closed)
-    print(util_obj.grid_obj.path_cost)
-    for i in range(len(grid_obj.graph)):
-        for j in range(len(grid_obj.graph[0])):
-            print("*", end=" ") if (i, j) in final_path else print("0", end=" ")
-        print()
-
-
-if __name__ == '__main__':
-    main()
+print("\nPredictive Parsing Table for Grammar G2:")
+pp_table_g2.print_parsing_table()
